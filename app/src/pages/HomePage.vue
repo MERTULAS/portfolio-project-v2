105b858348fa772fe637adeed7a5c48772b4c2a7
MERTULAS <h.mert.ulas@gmail.com>
2023-07-27 23:11:16 +0300

    main menu glitch effect on hover

--- app/src/pages/HomePage.vue
+++ app/src/pages/HomePage.vue
@@ -14,6 +14,10 @@ import { onMounted } from 'vue';
     {
       title: 'About',
       path: '/about'
+    },
+    {
+      title: 'Contact',
+      path: '/contact'
     }
   ]
 
@@ -46,8 +50,6 @@ import { onMounted } from 'vue';
     menuElementContainer.addEventListener('mousemove', function(e) {
       mouseOnTheMenu.update(e);
       menuElement.style.transform = menuTransformStyleUpdate(mouseOnTheMenu.y / (menuElement.offsetHeight >> 1), mouseOnTheMenu.x / (menuElement.offsetWidth >> 1));
-
-      console.log(menuElement.style.transform);
     });
          
   })
@@ -56,8 +58,19 @@ import { onMounted } from 'vue';
 
 <template>
   <nav>
-    <ul>
-      <li v-for="menuItem in mainMenu" :key="menuItem.title" @click="() => {}">{{ menuItem.title }}</li>
+    <ul class="main-menu">
+      <li 
+        v-for="menuItem in mainMenu" 
+        :key="menuItem.title" 
+        @click="() => {}" 
+        :data-title="menuItem.title"
+      >
+        <div class="glitch-effect-stick"></div>
+        {{ menuItem.title }}
+      </li>
+    </ul>
+    <ul class="back-shadow-menu">
+      <li v-for="menuItem in mainMenu" :key="menuItem.title">{{ menuItem.title }}</li>
     </ul>
   </nav>
   <div>
@@ -72,20 +85,137 @@ import { onMounted } from 'vue';
 }
 
 nav {
-  font-size: 8vmax;
   padding: 40px 100px;
-  perspective: 100px;
+  perspective: 200px;
+  display: flex;
+  align-items: center;
+}
+
+nav ul {
+  flex: 1;
+  /* opacity: .5; */
 }
 
 li {
   cursor: pointer;
-  margin: 20px;
+  font-size: 6vmax;
+  line-height: 1;
   text-transform: uppercase;
   transition: .5s all;
+  padding: 20px;
+  /* text-shadow: 15px 5px rgba(192, 64, 64, .5); */
 }
 
-li:hover {
+.main-menu li:hover {
   color: rgb(48, 48, 48);
+}
+
+.main-menu li {
+  position: relative;
+  width: fit-content;
+}
+
+.back-shadow-menu {
+  position: absolute;
+  background: transparent;
+  color: rgba(202, 83, 83, 0.9);
+  z-index: -1;
+}
+
+
+.main-menu li:hover::after,
+.main-menu li:hover::before {
+  content: attr(data-title);
+  position: absolute;
+  font-size: 6vmax;
+  overflow: hidden;
+}
+
+.main-menu li:hover::before {
+    left: 20.5px;
+    height: 0px;
+    color: rgba(255, 255, 255, 0.9);
+    z-index: 2;
+    animation: redShadow .7s ease-in infinite;
+    text-shadow: 8px 0 0 red;
+}
+
+.main-menu li:hover::after {
+    left: 17px;
+    color: rgba(255, 255, 255, 0.8);
+    z-index: 3;
+    background: rgba(0, 0, 0, 0.9);
+    animation: redHeight 1s ease-out infinite;
+    text-shadow: -5px 0 0 cyan;
+    mix-blend-mode: darken;
+}
+
+.glitch-effect-stick {
+    display: none;
+    position: absolute;
+    width: calc(100% - 40px);
+    height: 3px;
+    background: rgba(0, 0, 0, 1);
+    z-index: 4;
+    animation: whiteMove 2s ease-out infinite;
+}
+
+.main-menu li:hover .glitch-effect-stick {
+  display: block;
+}
+
+@keyframes redShadow {
+    20% {
+        height: 30%;
+    }
+    60% {
+        height: 6%;
+    }
+    100% {
+        height: 89%;
+    }
+}
+
+@keyframes redHeight {
+    20% {
+        height: 42%;
+    }
+    35% {
+        height: 12%;
+    }
+    50% {
+        height: 40%;
+    }
+    60% {
+        height: 20%;
+    }
+    70% {
+        height: 95%;
+    }
+    80% {
+        height: 54%;
+    }
+    100% {
+        height: 100%;
+    }
+}
+
+@keyframes whiteMove {
+    8% {
+        top: 38%;
+    }
+    14% {
+        top: 10%;
+    }
+    20% {
+        top: 42%;
+    }
+    32% {
+        top: 15%;
+    }
+    99% {
+        top: 90%;
+    }
 }
 
 </style>
