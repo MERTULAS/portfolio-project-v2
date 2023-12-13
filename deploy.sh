191e4e04e8557dfd52db8f0d7a9a6cf8e1d89164
MERTULAS <h.mert.ulas@gmail.com>
2023-12-13 11:23:51 +0300

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:MERTULAS/portfolio-project-v2.git master:gh-pages
+git push -f https://github.com/MERTULAS/portfolio-project-v2.git master:gh-pages
 
 cd -
