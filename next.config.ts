e478a9c107fe69167d7010302bd4d0b2355a1993
MERTULAS <h.mert.ulas@gmail.com>
2025-03-29 01:26:12 +0300

    next js migration - next build output set as dist

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: 'dist',
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
